e43cff06650a5ab70aa04c847f30498c98e93326
Gantogtokh0956 <gantogtokh0221@gmail.com>
2024-02-05 20:30:03 +0800

    kk

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,28 +1,34 @@
+
+import Head from "next/head";
 import { Footer } from "@/components/Footer"
 import { Header } from "@/components/Header"
 import { useEffect ,useState} from "react"
 import { useRouter } from 'next/router'
 import { Loader } from "@/components/Loader"
 
-export default function  Cole(){
-    const [details, setDetails] = useState([])
-    const router = useRouter()
+export default function  Cole({details}){
+    // const [details, setDetails] = useState([])
+    // const router = useRouter()
 
     
-    useEffect(()=>{
-        if(router.query.username && router.query.slug){
-            fetch(`https://dev.to/api/articles/${router.query.username}/${router.query.slug}`)
-            .then((response)=>response.json())
-            .then((data)=>{
-                setDetails(data)
-            })
-        }
+    // useEffect(()=>{
+    //     if(router.query.username && router.query.slug){
+    //         fetch(`https://dev.to/api/articles/${router.query.username}/${router.query.slug}`)
+    //         .then((response)=>response.json())
+    //         .then((data)=>{
+    //             setDetails(data)
+    //         })
+    //     }
         
-    },[router.query])
+    // },[router.query])
 
-    if(details === undefined) return <Loader/>
-
+    // if(details === undefined) return <Loader/>
     return <div>
+        <Head>
+            <meta property="og:title" content={details.title} />
+            <meta property="og:image" content={details.cover_image} />
+            <meta property="og:description" content={details.description} />
+      </Head>
         <div>
             <Header/>
         </div>
@@ -34,5 +40,9 @@ export default function  Cole(){
             <Footer/>
         </div>
     </div>
-    
 }
+export async function getServerSideProps({ query }) {
+    const res = await fetch(`https://dev.to/api/articles/${query.username}/${query.slug}`);
+    const details = await res.json();
+    return { props: { details } };
+  }
